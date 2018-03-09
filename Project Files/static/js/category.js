8fe3b1dd48d5513cb5dd74cd47ce2e912a9437c7
Alec Trievel <atrievel@gmail.com>
2018-03-08 21:36:39 -0500

    Updated categories.html to add new category

--- Project Files/static/js/category.js
+++ Project Files/static/js/category.js
@@ -4,6 +4,8 @@ function postNewCategory(name, description) {
         description: description.trim()
     });
 
+    console.log(new_cat);
+
     let jqxhr = $.ajax({
             url: "/categories",
             type: "POST",
@@ -22,15 +24,44 @@ function postNewCategory(name, description) {
             switch (jqxhr.status) {
                 case 200:
                     console.log("All good");
+
+                    swal({
+                        type: 'success',
+                        title: 'Awesome!',
+                        text: 'Your new category was created',
+                        footer: "<a href='/catgeory?category_id=?'>Click here to go to this category</a>",
+                      });
+
                     break;
                 case 403:
                     console.log("Category name is taken");
+
+                    swal({
+                        type: 'error',
+                        title: 'Oh no!',
+                        text: 'That catgeory already exists.',
+                      });
+
                     break;
                 case 405:
                     console.log("Try connecting to Flask first. Or someone didn't add the POST method to this route");
+
+                    swal({
+                        type: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong. Try submitting the request again',
+                      });
+
                     break;
                 case 500: 
                     console.log("Backend team broke something");
+
+                    swal({
+                        type: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong. Try submitting the request again',
+                      });
+
                     break;
                 default:
                     //probably won't need this in production, but here's where we get the response (undefined if error)
