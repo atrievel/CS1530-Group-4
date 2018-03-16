1ee852f1e29b8d0c30b76da3a9e86ff82f1c2b77
Alec Trievel <atrievel@gmail.com>
2018-03-16 17:58:25 -0400

    Updated imports list

--- Project Files/imports.py
+++ Project Files/imports.py
@@ -1,6 +1,7 @@
 import config
-from flask import Flask, request, redirect, render_template, url_for, flash, jsonify
+from flask import Flask, request, redirect, render_template, url_for, flash, jsonify, session
 from datetime import datetime
 from functools import wraps
 from flask.ext.bcrypt import Bcrypt
 from database import db, User, Category, Thread, Comment, Friendship, CommentVote, ThreadVote, Message
+import os
