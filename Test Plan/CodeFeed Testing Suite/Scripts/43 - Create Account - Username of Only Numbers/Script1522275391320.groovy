b86c5c3e6d56c2f087d1f6c65f5c0edfb785dc92
Jeremy Kato <jeremykato@hotmail.com>
2018-03-29 03:25:58 -0400

    Scripts

---
+++ Test Plan/CodeFeed Testing Suite/Scripts/43 - Create Account - Username of Only Numbers/Script1522275391320.groovy
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://localhost:5000/register')
+
+WebUI.setText(findTestObject('Page_Register  CodeFeed/input_name'), 'test test')
+
+WebUI.setText(findTestObject('Page_Register  CodeFeed/input_email'), 'test@example.com')
+
+WebUI.setText(findTestObject('Page_Register  CodeFeed/input_username'), '43')
+
+WebUI.setText(findTestObject('Page_Register  CodeFeed/input_password'), '123456789')
+
+WebUI.setText(findTestObject('Page_Register  CodeFeed/input_verify_password'), '123456789')
+
+WebUI.click(findTestObject('Page_Register  CodeFeed/input_btn btn-lg btn-primary m'))
+
+WebUI.delay(2)
+
+url = WebUI.getUrl()
+
+WebUI.verifyEqual(url, 'http://localhost:5000/register')
+
+WebUI.closeBrowser()
+
---
+++ Test Plan/CodeFeed Testing Suite/Scripts/71 - Category - Add Category With Description Over 60 Chars/Script1522288006941.groovy
@@ -0,0 +1,52 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://localhost:5000/login')
+
+WebUI.setText(findTestObject('Category Testing/input_username (1)'), 'test')
+
+WebUI.setText(findTestObject('Category Testing/input_password (1)'), '123456789')
+
+WebUI.click(findTestObject('Category Testing/input_login'))
+
+WebUI.click(findTestObject('Category Testing/a_Categories (1)'))
+
+WebUI.click(findTestObject('Category Testing/button_New category'))
+
+WebUI.setText(findTestObject('Category Testing/input_txtCategoryName'), 'test71')
+
+WebUI.setText(findTestObject('Category Testing/textarea_txtDescription'), 'pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp')
+
+WebUI.click(findTestObject('Category Testing/button_Post Category'))
+
+WebUI.delay(3)
+
+WebUI.verifyElementNotPresent(findTestObject('Category Testing/div_i  Awesome'), 0)
+
+WebUI.verifyElementNotPresent(findTestObject('Category Testing/div_Your new category was crea (1)'), 0)
+
+WebUI.verifyElementNotPresent(findTestObject('Category Testing/button_OK (2)'), 0)
+
+WebUI.closeBrowser()
+
