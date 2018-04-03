d38c02a1e5a551bf0d7e4689239761614f5ed498
Alec Trievel <atrievel@gmail.com>
2018-04-03 15:47:27 -0400

    Added test case run files

--- Test Plan/CodeFeed Testing Suite/Test Suites/CodeFeedSuite.ts
+++ Test Plan/CodeFeed Testing Suite/Test Suites/CodeFeedSuite.ts
@@ -658,4 +658,376 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Profile/Login</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>dff21892-72df-4e4b-afa0-7bd81e121e85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/11 - Add Category While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8dee8cbd-8323-4038-8f99-289d0b439d30</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/13 - Add Thread While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f9b1ab6b-19b2-4f30-8467-4f0a3f818865</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/15 - Add Comment While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0cb8dda8-99ed-436f-a9b1-e49ab667f7c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/17 - Vote on Thread While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>949ccaab-1455-4162-8323-feae8133cf1f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/22 - View Landing Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>83449c76-f5ab-4dab-aaf8-c00296affde5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/23 - Click Register Button</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f7c8a0b-bc86-45ab-8629-485b41b216f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/24 - Click Login Button</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd45c88d-25ae-44e2-8ef2-8b7737fdbe08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/25 - View Registered User Count</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db748310-840e-4c2e-bc1b-eeabfe4ca4a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/27 - View Group Count</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f841ae11-2558-474f-99ee-f45e91ecfe91</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/34 - Create Account - Correct Name Format</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9175382d-ba3f-4c54-b38a-5d38e723ad3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/35 - Create Account - Name is only Numbers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6f3fb34-3b24-4210-8d00-5b0fb5e3f27a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/36 - Create Account - Name is Only Special Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93856d50-b5a6-48b0-ae97-9b99e86d7a9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/37 - Create Account - Name is Only One Character</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f40a834c-ab53-442c-af6d-1fbb667225da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/38 - Create Account - Name is 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>453127c3-7bae-485b-b4fb-a5a3ca5c3b94</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/39 - Create Account - Name is over 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8970a8b7-3986-4418-9f68-9442632dd71d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/4 - View Profile While Not Logged In</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3b210734-3dc1-44d7-b68b-28fdfde98c27</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38aa699f-0974-4901-a8dc-bbd727818188</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/40 - Create Account - Valid Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bd0c3ca8-7826-44f6-ae1c-4b4a91a343d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/41 - Create Account - Invalid Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>85b4377c-4188-4a53-a7fc-0ff8d1384a88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/42 - Create Account - Username Of Only Letters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8a9a2fe-c353-4dcb-9a9d-2d384389c230</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/43 - Create Account - Username of Only Numbers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>853f2b5a-7ae6-41e0-a9c5-6af609fe3074</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/44 - Create Account - Username is Only Special Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fff971e4-bba3-4244-9f5e-2e7188381bac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/45 - Create Account - Username is Only 1 Character</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86a3b153-729d-4797-9a02-8f64cf48de1e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/46 - Create Account - Username is 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d63f949f-3973-450f-abaa-a32c2e402230</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/47 - Create Account - Username Is Over 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9b6daf0-f9df-4d67-acbe-944f25c96aab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/48 - Create Account - Password is Less Than 8 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e094c4e-112e-47b0-9fa6-ad1fdb0d1801</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/49 - Create Account - Password is 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b62b6b18-7136-42d8-9709-40b125c09498</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/5 - View Profile While Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3db0657d-7ed5-4841-bcdc-2b62d2febe13</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/50 - Create Account - Password is 64 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>daae3ebf-1904-4e2c-a5a5-81000a465545</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/51 - Create Account - Passwords do not match</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78509984-c8d7-4a82-b9c7-147b99263181</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/53 - Logout While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3e72906-c3ec-4006-aac6-99fe05dd046a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/54 - Register Account with Taken Username</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef5a6988-ca3c-43df-a0ea-d42481ed1385</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/55 - Click Register Button on Login Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f18ee4a8-7508-4e0d-9026-7a34e7ee1ccc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/56 - Login with Matching Username and Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8d45e30-4b78-41c5-a574-6e3ebd23cf89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/57 - Login With Wrong Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54e43fdc-9e76-4e67-a14d-caece95f32ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/58 - Login with Invalid Username and Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>789dffae-defc-44a5-aa76-a1e411c7c3e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/59 - Categories Navbar Loads Correctly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73380d14-0c9f-4b0a-8fe4-310f38c926f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/6 - Load Messages While Logged In</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6ceb3e38-472c-42c3-a6f7-167c2836ab5b</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9905d5cb-683e-4af0-948f-03009d414773</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/60 - Category Title Loads Properly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96e2389f-ebdf-4c1d-a9bb-c410e8ae3ff2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/64 - Category Search Shows No Results Found</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>91209f89-f6e1-4a10-87b8-da8bb9bb5d45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/65 - Category Search Shows Test Result</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>63537a54-2c2c-431d-af45-a14a714300cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/66 - Category - Adding Duplicate Category</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2cc8b01-d6c1-4d82-85da-18931df75c53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/67 - Category - Add Category Without Title</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>824b5132-243b-4dc7-ac68-aa57e41afa4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/68 - Category - Add Category With Title Greater Than 20 Characters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6bfde68-67c8-474e-acb5-4fba5bb1878d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/69 - Category - Add Category With Title Less than 20 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7bf12f37-ea48-44c0-bc20-1ec7a2f396ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/7 - View Messages While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>501c0aa3-4042-494a-88e6-733cace96c5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/70 - Category - Add Category with Blank Description</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0425c6b9-4101-4b28-a1a2-d64bc54ca9b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/71 - Category - Add Category With Description Over 60 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed73db2d-67a4-465f-a6f8-2604e4afa04b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/74 - Thread Navbar Loads Correctly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88184d38-f29f-4bb9-a152-6fc1a8fc6f54</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/75 - Thread Title Loads Correcctly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e83281d-523b-4ee7-8dfc-bf3da2c2c7d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/78 - Thread - Search for Nonexistant Thread</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2502228c-2841-4a79-92be-5acaa88c2098</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/79 - Thread - Search for Existing Thread</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>56c1d900-2247-495d-8724-ff1bdad5f4d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/80 - Thread - Add Thread With Blank Title</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69701e50-d83d-45ef-8346-d43a77f4cb34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/81 - Thread - Add Thread With Title Greater than 20 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a741c7c-2016-4c50-a7da-35c1cf63d9af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/82 - Thread - Add Thread With Title Less Than 20 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>165c2699-a678-4a46-8bec-db05204a0de0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/83 - Thread - Add Thread With Empty Body</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cf50fd90-beb8-4765-bfb0-ba36bd741ea8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/84 - Thread - Add Thread With More Than 60 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>30d5dd75-4f14-4707-9e07-5ba2e03adf0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/87 - Thread - Upvote Thread</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d94fa813-cf85-4f46-8cc8-b2c7d292ceff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/9 - Send Friend Request While Not Logged In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b26e89f-317e-4875-b9ac-a03ea425179b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/Create User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>224aaa90-f27f-4391-96c7-b7b18b2a01fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JeremyFolder/Login</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Plan/CodeFeed Testing Suite/Test Suites/Jeremy - Run Everything.ts
+++ Test Plan/CodeFeed Testing Suite/Test Suites/Jeremy - Run Everything.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Run Everything</name>
+   <name>Jeremy - Run Everything</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
